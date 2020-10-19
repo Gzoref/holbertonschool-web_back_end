74627da3d5346f14da3eaf484e3bbdc129867bb2
Gzoref <gzoref@gmail.com>
2020-10-19 15:03:51 -0400

    import math

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+import math
+'''
+Basic annotations - floor
+'''
+
+
+def floor(n: float) -> float:
+    '''
+    Finds the floor of a float
+    '''
+    return math.floor(n)
