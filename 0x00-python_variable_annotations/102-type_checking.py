a993bcf056af33ecd4fe437810b2eab2dc0693da
Gzoref <gzoref@gmail.com>
2020-10-19 18:15:21 -0400

    Type check 1st arg

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -7,12 +7,12 @@ Type checking
 from typing import Iterable, List, Sequence, Tuple, Union, Any, TypeVar
 
 
-def zoom_array(lst: List, factor: int = 2) -> List:
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
     '''
     Zoom array ???
     '''
     zoomed_in: List = [
-        item for item in list(lst)
+        item for item in lst
         for i in range(factor)
     ]
     return zoomed_in
@@ -20,6 +20,6 @@ def zoom_array(lst: List, factor: int = 2) -> List:
 
 array = [12, 72, 91]
 
-zoom_2x = zoom_array(array)
+zoom_2x = zoom_array(tuple(array))
 
-zoom_3x = zoom_array(array, int(3.0))
+zoom_3x = zoom_array(tuple(array), int(3.0))
