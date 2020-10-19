64f7c6bfe16afbd970b7194287c576c830eaae58
Gzoref <gzoref@gmail.com>
2020-10-19 15:31:38 -0400

    Mixed list

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+from typing import Union, List
+
+'''
+Complex types - mixed list
+'''
+
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    '''
+    Returns string representation of float
+    '''
+    return sum(mxd_lst)
