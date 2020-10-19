0742dab72d84ca40c2a9b35b6889092d1cc1f17d
Gzoref <gzoref@gmail.com>
2020-10-19 15:32:34 -0400

    Move import below comment

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 
-from typing import Union, List
-
 '''
 Complex types - mixed list
 '''
+
+from typing import Union, List
 
 
 def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
