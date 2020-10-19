3ee031e5d78872eee9ef37d019dd0e82748956f5
Gzoref <gzoref@gmail.com>
2020-10-19 15:42:50 -0400

    Fix return statement

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -4,10 +4,10 @@
 Complex types - string and int/float to tuple
 '''
 
-from typing import Union
+from typing import Union, Tuple
 
 
-def to_kv(k: str, v: Union[int, float]) -> tuple:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     '''
     Returns string representation of float
     '''
