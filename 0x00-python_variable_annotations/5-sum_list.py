150c1f512a7062c1b0cadb40c0c4a9344ddb9e5f
Gzoref <gzoref@gmail.com>
2020-10-19 15:33:32 -0400

    Move import below comment

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 
-import typing
-
 '''
 Complex types - list of floats
 '''
 
+from typing import List
 
-def sum_list(input_list: typing.List[float]) -> float:
+
+def sum_list(input_list: List[float]) -> float:
     '''
     Returns sum of floats
     '''
