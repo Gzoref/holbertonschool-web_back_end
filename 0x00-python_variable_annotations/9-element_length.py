c0c685bcca1831c51314c0e4e30d17905259367c
Gzoref <gzoref@gmail.com>
2020-10-19 16:38:33 -0400

    Duck Type and iterable

---
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+'''
+Duck Typing
+'''
+
+from typing import Iterable, List, Sequence, Tuple
+
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    return [(i, len(i)) for i in lst]
