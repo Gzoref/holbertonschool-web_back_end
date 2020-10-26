812966a1cce2145b2704d0a55a108ddd01a7b01e
Gzoref <gzoref@gmail.com>
2020-10-26 15:58:22 -0400

    Change to MRU algo

--- 0x03-caching/4-mru_cache.py
+++ 0x03-caching/4-mru_cache.py
@@ -17,27 +17,32 @@ class MRUCache(BaseCaching):
         Initialize
         """
         self.queued_item = deque()
+        self.lru_item = []
         super().__init__()
 
     def put(self, key, item):
         """
-       Print or discard the least recently used item (LRU algorithm)
+       Print or discard the most recently used item (MRU algorithm)
         """
-        if key and item:
+        if key is not None and item is not None:
             if key in self.cache_data:
-                self.queued_item.remove(key)
-            elif len(self.queued_item) > BaseCaching.MAX_ITEMS:
-                popped_item = self.queued_item.pop()
-                del self.cache_data[popped_item]
-                print("DISCARD: " + str(popped_item))
-            self.queued_item.append(key)
-            self.cache_data[key] = item
+                self.cache_data[key] = item
+                self.lru_item.remove(key)
+            else:
+                if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                    del self.cache_data[self.lru_item[-1]]
+                    print("DISCARD:", self.lru_item[-1])
+                    self.lru_item.pop(-1)
+                self.cache_data[key] = item
+            self.lru_item.append(key)
 
     def get(self, key):
         """
         Return value of cache_data linked to key
         """
         if key in self.cache_data:
-            self.queued_item.remove(key)
-            self.queued_item.append(key)
+            self.lru_item.remove(key)
+            self.lru_item.append(key)
             return self.cache_data.get(key)
+        else:
+            return None
