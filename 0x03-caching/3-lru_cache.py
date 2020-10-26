3282c26b87e8e595c784e9220e7226d6b3995f2b
Gzoref <gzoref@gmail.com>
2020-10-26 13:27:24 -0400

    Run with list, remove deque

--- 0x03-caching/3-lru_cache.py
+++ 0x03-caching/3-lru_cache.py
@@ -17,27 +17,32 @@ class LRUCache(BaseCaching):
         Initialize
         """
         self.queued_item = deque()
+        self.lru_item = []
         super().__init__()
 
     def put(self, key, item):
         """
        Print or discard the least recently used item (LRU algorithm)
         """
-        if key and item:
+        if key is not None and item is not None:
             if key in self.cache_data:
-                self.queued_item.remove(key)
-            elif len(self.queued_item) > BaseCaching.MAX_ITEMS:
-                popped_item = self.queued_item.popleft()
-                del self.cache_data[popped_item]
-                print("DISCARD: " + str(popped_item))
-            self.queued_item.append(key)
-            self.cache_data[key] = item
+                self.cache_data[key] = item
+                self.lru_item.remove(key)
+            else:
+                if len(self.cache_data) >= self.MAX_ITEMS:
+                    del self.cache_data[self.lru_item[0]]
+                    print("DISCARD:", self.lru_item[0])
+                    self.lru_item.pop(0)
+                self.cache_data[key] = item
+            self.lru_item.append(key)
 
     def get(self, key):
         """
         Return value of cache_data linked to key
         """
         if key in self.cache_data:
-            self.queued_item.remove(key)
-            self.queued_item.append(key)
+            self.lru_item.remove(key)
+            self.lru_item.append(key)
             return self.cache_data.get(key)
+        else:
+            return None
