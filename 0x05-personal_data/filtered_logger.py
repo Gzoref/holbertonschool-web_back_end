60492e6b229f268aa2b8557608cc8252104a0dc0
Gzoref <gzoref@gmail.com>
2020-10-31 14:00:19 -0400

    Log formatter

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -5,6 +5,7 @@ Filter personal information with Regex
 
 from re import sub
 from typing import List
+import logging
 
 
 def filter_datum(fields: List[str],
@@ -18,3 +19,24 @@ def filter_datum(fields: List[str],
         pattern = item + "=.+?(?=abc)*\\" + ";"
         message = sub(pattern, item + "=" + redaction + separator, message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        self.fields = fields
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+
+    def format(self, record: logging.LogRecord) -> str:
+        result = logging.Formatter(self.FORMAT).format(record)
+        return filter_datum(
+            self.fields,
+            self.REDACTION,
+            result,
+            self.SEPARATOR)
