4539cf7e7aff0788f99054b07c692537e3a2092b
Gzoref <gzoref@gmail.com>
2020-11-03 17:04:59 -0500

    Shorten param names and Base64 decode method

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -4,6 +4,7 @@
 Template for authentication system
 """
 
+from base64 import b64decode
 from api.v1.auth.auth import Auth
 import base64
 
@@ -13,28 +14,25 @@ class BasicAuth(Auth):
     Extends Auth class
     """
 
-    def extract_base64_authorization_header(
-            self, authorization_header: str) -> str:
+    def extract_base64_authorization_header(self, auth_header: str) -> str:
         """Returns the decoded value of a Base64
            string base64_authorization_header
         """
-        if authorization_header is None or not isinstance(
-                authorization_header, str):
+        if auth_header is None or not isinstance(auth_header, str):
             return None
-        if 'Basic ' not in authorization_header:
+        if 'Basic ' not in auth_header:
             return None
-        return authorization_header[6:]
+        return auth_header[6:]
 
-    def decode_base64_authorization_header(
-            self, base64_authorization_header: str) -> str:
+    def decode_base64_authorization_header(self, b64_auth_header: str) -> str:
         """ Returns the Base64 part of the Authorization
             header for a Basic Authentification
         """
-        if base64_authorization_header is None or not isinstance(
-                base64_authorization_header, str):
+        if b64_auth_header is None or not isinstance(b64_auth_header, str):
             return None
         try:
-            return base64.b64encode(base64.b64decode(
-                base64_authorization_header)) == base64_authorization_header
+            b64 = base64.b64decode(b64_auth_header)
+            b64_decode = b64.decode('utf-8')
         except Exception:
             return None
+        return b64_decode
