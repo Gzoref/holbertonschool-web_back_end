0b739cdca8901cfe811766e750c29acabb4b891b
Gzoref <gzoref@gmail.com>
2020-11-03 16:11:53 -0500

    Request validation!

--- 0x06-Basic_authentication/api/v1/app.py
+++ 0x06-Basic_authentication/api/v1/app.py
@@ -2,6 +2,7 @@
 """
 Route module for the API
 """
+from api.v1.auth.auth import Auth
 from os import getenv
 
 from werkzeug.exceptions import Unauthorized
@@ -14,6 +15,12 @@ import os
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+
+auth = None
+
+if os.getenv('AUTH_TYPE') == 'auth':
+    from api.v1.auth.auth import Auth
+    auth = Auth()
 
 
 @app.errorhandler(404)
@@ -29,11 +36,31 @@ def unauthorized(error) -> str:
     """
     return jsonify({"error": "Unauthorized"}), 401
 
+
 @app.errorhandler(403)
 def unauthorized(error) -> str:
     """ Request Forbidden handler
     """
     return jsonify({"error": "Forbidden"}), 403
+
+
+def before_request() -> None:
+    """ Filter each request
+    """
+    request_path_list = [
+        '/api/v1/status/',
+        '/api/v1/unauthorized/',
+        '/api/v1/forbidden/']
+
+    if auth:
+        if auth.require_auth(request.path, request_path_list):
+            if auth.authorization_header(request) is None:
+                abort(401)
+            if auth.current_user(request) is None:
+                abort(403)
+
+
+app.before_request(before_request)
 
 
 if __name__ == "__main__":
