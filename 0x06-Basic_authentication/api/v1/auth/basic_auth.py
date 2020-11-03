cdd43563ffae7c29d05e261e7d88c7c4110df172
Gzoref <gzoref@gmail.com>
2020-11-03 18:48:15 -0500

    Basic - User Object

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -4,8 +4,10 @@
 Template for authentication system
 """
 
+from typing import TypeVar
 from base64 import b64decode
 from api.v1.auth.auth import Auth
+from models.user import User
 import base64
 
 
@@ -46,3 +48,21 @@ class BasicAuth(Auth):
                 or ':' not in decoded_b64_auth_header:
             return (None, None)
         return decoded_b64_auth_header.split(':')
+
+    def user_object_from_credentials(
+            self,
+            user_email: str,
+            user_pwd: str) -> TypeVar('User'):
+        """ Returns the User instance based on his email and password.
+        """
+        if user_email is None or not isinstance(
+                user_email, str) or user_pwd is None or not isinstance(
+                user_pwd, str):
+            return None
+        users = User.search({'email': user_email})
+
+        for user in users:
+            if not user.is_valid_password(user_pwd):
+                return None
+            else:
+                return user
