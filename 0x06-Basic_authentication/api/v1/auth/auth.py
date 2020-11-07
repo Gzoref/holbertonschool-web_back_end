c099a5a09ae3b99a6a3b90d80971ca0f555c96cb
Gzoref <gzoref@gmail.com>
2020-11-06 23:16:21 -0500

    Require auth with stars

--- 0x06-Basic_authentication/api/v1/auth/auth.py
+++ 0x06-Basic_authentication/api/v1/auth/auth.py
@@ -23,6 +23,14 @@ class Auth():
             path += '/'
         if excluded_paths[-1] != '/':
             excluded_paths += '/'
+
+        astericks = [stars[:-1]
+                     for stars in excluded_paths if stars[-1] == '*']
+
+        for stars in astericks:
+            if path.startswith(stars):
+                return False
+
         if path in excluded_paths:
             return False
         return True
