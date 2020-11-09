4df98ddff59076a452c8d4a232219cfb5868762c
Gzoref <gzoref@gmail.com>
2020-11-09 15:48:50 -0500

    Create SessionAuth class

--- 0x07-Session_authentication/api/v1/app.py
+++ 0x07-Session_authentication/api/v1/app.py
@@ -6,7 +6,6 @@ from os import getenv
 
 from werkzeug.exceptions import Unauthorized
 from api.v1.views import app_views
-from api.v1.auth import auth
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
@@ -24,6 +23,9 @@ if os.getenv('AUTH_TYPE') == 'auth':
 elif os.getenv('AUTH_TYPE') == 'basic_auth':
     from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
+elif os.getenv('AUTH_TYPE') == 'session_auth':
+    from api.v1.auth.session_auth import SessionAuth
+    auth = SessionAuth()
 
 
 @app.errorhandler(404)
