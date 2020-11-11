e6b3c2bbcb6e87cf4b93de9f65c7f267e4b16323
Gzoref <gzoref@gmail.com>
2020-11-11 15:22:02 -0500

    Put everything back to hwo it was 2 commits ago

--- 0x07-Session_authentication/api/v1/auth/session_exp_auth.py
+++ 0x07-Session_authentication/api/v1/auth/session_exp_auth.py
@@ -52,11 +52,11 @@ class SessionExpAuth(SessionAuth):
         if self.session_duration <= 0:
             return session_dictionary["user_id"]
 
-        created_at = session_dictionary['created_at']
-        if created_at is None:
+        if "created_at" not in session_dictionary:
             return None
-        
+
         create_time = session_dictionary["created_at"]
+
         time_delta = timedelta(seconds=self.session_duration)
         if (create_time + time_delta) < datetime.now():
             return None
