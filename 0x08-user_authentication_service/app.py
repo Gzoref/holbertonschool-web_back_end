73b29a1360d017077de1b45d58ce360fc0df0f3f
Gzoref <gzoref@gmail.com>
2020-11-13 17:00:17 -0500

    Create route to register a user

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python3
-from flask import Flask, jsonify
 
 """Route module for the API
 """
+
+from flask import Flask, jsonify,request
+from auth import Auth
+
+
+AUTH = Auth()
 
 app = Flask(__name__)
 
@@ -10,9 +15,26 @@ app = Flask(__name__)
 @app.route('/', methods=['GET'], strict_slashes=False)
 def welcome() -> str:
     """ GET /
-        Return welcome message
+    Return:
+        welcome message
     """
     return jsonify({"message": "Bienvenue"})
+
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def register_users():
+    """ POST /users
+        JSON body:
+            -email
+            -password
+       return:
+       An endpoint to register a user
+    """
+    user_request = request.form
+    try:
+        user = AUTH.register_user(user_request['email'], user_request['password'])
+        return jsonify({"email": user.email, "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
