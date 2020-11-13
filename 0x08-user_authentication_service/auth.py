431ec2163055123f16b68406abb6848f2a50b48b
Gzoref <gzoref@gmail.com>
2020-11-13 16:08:11 -0500

    Create Auth class

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -1,8 +1,20 @@
+from db import DB
 import bcrypt
 
+""" Auth class to validate user attributes
+"""
 
 def _hash_password(password: str) -> str:
-    """
-    Takes in a password and returns a salted hash.
+    """ Takes in a password and returns a salted hash.
     """
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        """ Initailizes instance of DB
+        """
+        self._db = DB()
+
