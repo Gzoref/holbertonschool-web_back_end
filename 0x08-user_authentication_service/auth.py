0d0a300aa2c3eec1eee20d93eb3c24c5efafd497
Gzoref <gzoref@gmail.com>
2020-11-13 16:25:22 -0500

    Create method to register a new user and hash password

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -1,13 +1,21 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
 from db import DB
+from user import User
 import bcrypt
 
 """ Auth class to validate user attributes
 """
 
+
 def _hash_password(password: str) -> str:
     """ Takes in a password and returns a salted hash.
     """
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
 
 class Auth:
     """Auth class to interact with the authentication database.
@@ -18,3 +26,13 @@ class Auth:
         """
         self._db = DB()
 
+    def register_user(self, email: str, password: str) -> User:
+        """ Returns a User object, or ValueError with a message
+        """
+        try:
+            self._db.find_user_by(email=email)
+            raise ValueError("User {} already exists.".format(email))
+        except NoResultFound:
+            hashed_password = _hash_password(password)
+            new_user = self._db.add_user(email, hashed_password)
+            return new_user
