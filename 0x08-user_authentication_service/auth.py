36b5958bb8c6f0426b645ba0abdf0686337787f4
Gzoref <gzoref@gmail.com>
2020-11-13 19:48:28 -0500

    Remove empty line

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -43,7 +43,6 @@ class Auth:
         """
         try:
             user = self._db.find_user_by(email=email)
-
             if bcrypt.checkpw(password.encode('utf-8'), user.hashed_password):
                 return True
         except NoResultFound:
