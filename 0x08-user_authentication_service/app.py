0d14ab5e1d780dacc26c4bc7983156e58710c425
Gzoref <gzoref@gmail.com>
2020-11-14 12:43:50 -0500

    Create a route to return user email

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -78,5 +78,19 @@ def logout():
     return redirect('/')
 
 
+@app.route('/profile', methods=['GET'], strict_slashes=False)
+def profile() -> str:
+    """ GET /profile
+    Return:
+        - Use session_id to find the user.
+        - 403 if session ID is invalid
+    """
+    user_cookie = request.cookies.get("session_id")
+    user = AUTH.get_user_from_session_id(user_cookie)
+    if not user:
+        abort(403)
+    return jsonify({"email": user}), 200
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
