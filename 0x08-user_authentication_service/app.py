f5502ddb870d3fcd612fbb3f335883fed324cb26
Gzoref <gzoref@gmail.com>
2020-11-14 11:34:41 -0500

    Create login route

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -3,8 +3,10 @@
 """Route module for the API
 """
 
-from flask import Flask, jsonify,request
+from flask import Flask, jsonify, request, abort
+from flask.helpers import make_response
 from auth import Auth
+from user import User
 
 
 AUTH = Auth()
@@ -20,6 +22,7 @@ def welcome() -> str:
     """
     return jsonify({"message": "Bienvenue"})
 
+
 @app.route('/users', methods=['POST'], strict_slashes=False)
 def register_users():
     """ POST /users
@@ -31,10 +34,33 @@ def register_users():
     """
     user_request = request.form
     try:
-        user = AUTH.register_user(user_request['email'], user_request['password'])
+        user = AUTH.register_user(
+            user_request['email'],
+            user_request['password'])
         return jsonify({"email": user.email, "message": "user created"})
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
+def login():
+    """ POST /sessions
+        - email
+        - password
+        Return:
+        Data from "email" and "password" fields
+    """
+    user_request = request.form
+
+    user_email = user_request.get('email', '')
+    user_password = user_request.get('password', '')
+    valid_log = AUTH.valid_login(user_email, user_password)
+    if not valid_log:
+        abort(401)
+    response = make_response(
+        jsonify({"email": user_email, "message": "logged in"}))
+    response.set_cookie('session_id', AUTH.create_session(user_email))
+    return response
 
 
 if __name__ == "__main__":
