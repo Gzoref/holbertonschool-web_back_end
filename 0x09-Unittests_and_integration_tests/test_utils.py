f3b352be21809a30175fac8aba278c9dfd4c9e63
Gzoref <gzoref@gmail.com>
2020-11-20 17:50:46 -0500

    Simplify test_get_json test

--- 0x09-Unittests_and_integration_tests/test_utils.py
+++ 0x09-Unittests_and_integration_tests/test_utils.py
@@ -4,9 +4,10 @@
 """
 
 import unittest
-from utils import access_nested_map
+import requests
+from utils import access_nested_map, get_json
 from parameterized import parameterized
-
+from unittest.mock import Mock, patch
 
 class TestAccessNestedMap(unittest.TestCase):
     """ Test class for utils.access_nested_map
@@ -20,9 +21,28 @@ class TestAccessNestedMap(unittest.TestCase):
         """
         self.assertEqual(access_nested_map(nested_map, path), expected)
 
+
     @parameterized.expand([({}, ("a",), KeyError),
                            ({"a": 1}, ("a", "b"), KeyError)])
     def test_access_nested_map_exception(self, nested_map, path, expected):
         """ Test acesss_nested_map exception
         """
         self.assertRaises(expected)
+
+
+class TestGetJson(unittest.TestCase):
+    """ Test class for utils.get_json
+    """
+
+    @parameterized.expand([("http://example.com", {"payload": True}),
+                           ("http://holberton.io", {"payload": False})])
+    def test_get_json(self, test_url, test_payload):
+        """ Test get_json and create mock test
+        """
+        mock = Mock()
+        mock.json.return_value = test_payload
+        with patch('requests.get', return_value=mock):
+            get_json_response = get_json(test_url)
+            self.assertEqual(get_json_response, test_payload)
+            mock.json.assert_called_once()
+
