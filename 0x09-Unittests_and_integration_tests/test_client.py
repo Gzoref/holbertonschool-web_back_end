ff476762a848d05a0969a3bb38f99bf5cc345d03
Gzoref <gzoref@gmail.com>
2020-11-21 17:13:39 -0500

    Mocking a Property

--- 0x09-Unittests_and_integration_tests/test_client.py
+++ 0x09-Unittests_and_integration_tests/test_client.py
@@ -7,22 +7,32 @@ import unittest
 import requests
 from utils import access_nested_map, get_json, memoize
 from parameterized import parameterized
-from unittest.mock import Mock, patch
+from unittest.mock import Mock, patch, PropertyMock
 from utils import get_json
-from client import GithubOrgClient, org
+from client import GithubOrgClient
+import client
 
 
 class TestGithubOrgClient(unittest.TestCase):
     """ Test class for GithubOrgClient
     """
 
-    @patch('client.get_json')
     @parameterized.expand(
         [("google", {"google", True}), ("abc", {"abc", True})])
-    def test_org(self, org, expected, patch):
+    @patch('client.get_json')
+    def test_org(self, org, expected, get_patch):
         """ Test the GithubOrgClient.org method
         """
-        patch.return_value = expected
+        get_patch.return_value = expected
         gh_client = GithubOrgClient(org)
         self.assertEqual(gh_client.org, expected)
-        patch.assert_called_once_with(f'https://api.github.com/orgs/{org}')
+        get_patch.assert_called_once_with(f'https://api.github.com/orgs/{org}')
+
+    def test_public_repos_url(self):
+        """ Test the GithubOrgClient._public_repos_url
+        """
+        expected = "www.geoff.com"
+        with patch('client.GithubOrgClient.org',
+                   PropertyMock(return_value={'repos_url': expected})):
+            gh_client = GithubOrgClient("adobe")
+            self.assertEqual(gh_client._public_repos_url, expected)
