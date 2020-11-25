b18a4b4bc2283368ead2dc1968e2561005665f1b
Gzoref <gzoref@gmail.com>
2020-11-25 14:40:41 -0500

    Get locale from request

---
+++ 0x0A-i18n/2-app.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+"""Route module for the API
+"""
+
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config:
+    """ Available languages class
+    """
+
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def welcome() -> str:
+    """ GET /
+    Return:
+        Hello world
+    """
+    return render_template('1-index.html')
+
+
+@babel.localeselector
+def get_locale():
+    """ Determine best match for supported languages
+    """
+    return request.accept_languages.best_match(['en', 'fr'])
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
