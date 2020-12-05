db6d7a271d81d6cfc1205b1e56f8e95c13828801
Gzoref <gzoref@gmail.com>
2020-12-05 01:34:23 -0500

    Move DELIMITER from comment section

--- 0x0C-MySQL_Advanced/7-average_score.sql
+++ 0x0C-MySQL_Advanced/7-average_score.sql
@@ -1,5 +1,6 @@
- -- Creates a stored procedure ComputeAverageScoreForUser that computes AND store the average score for a student DELIMITER $$
+ -- Creates a stored procedure ComputeAverageScoreForUser that computes AND store the average score for a student 
 
+DELIMITER $$
 CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT) BEGIN UPDATE users
 
 SET average_score = (
