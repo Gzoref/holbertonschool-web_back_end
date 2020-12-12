e6185d053039d7aa102e5ced0bcf76a32cc92856
Gzoref <gzoref@gmail.com>
2020-12-12 17:02:24 -0500

    Create report object file

---
+++ 0x0E-ES6_basic/12-createReportObject.js
@@ -0,0 +1,10 @@
+export default function createReportObject(employeesList) {
+  return {
+    allEmployees: {
+      engineering: [
+        'John Doe',
+        'Guillaume Salva',
+      ],
+    },
+  };
+}
