616704da322fe78f11073e405139dcf63ed6b954
Gzoref <gzoref@gmail.com>
2020-12-16 11:51:25 -0500

    Remove function args

--- 0x0F-ES6_promise/100-await.js
+++ 0x0F-ES6_promise/100-await.js
@@ -1,6 +1,6 @@
 import { uploadPhoto, createUser } from './utils';
 
-export default async function asyncUploadUser(uploadPhoto, createUser) {
+export default async function asyncUploadUser() {
   try {
     const photo = await uploadPhoto();
     const user = await createUser();
