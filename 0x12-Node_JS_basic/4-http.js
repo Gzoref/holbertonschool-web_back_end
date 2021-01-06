ebb7e03b59bfe0685199ae7c69991e23233033bf
Gzoref <gzoref@gmail.com>
2021-01-05 19:12:30 -0500

    Export app as module

--- 0x12-Node_JS_basic/4-http.js
+++ 0x12-Node_JS_basic/4-http.js
@@ -1,6 +1,7 @@
 const app = require('http');
 
 app.createServer((request, result) => {
-  result.write('Hello Holberton School!');
-  result.end();
+  result.end('Hello Holberton School!');
 }).listen(1245);
+
+module.exports = app;
