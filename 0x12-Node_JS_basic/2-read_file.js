1dedcb3ae1941ff50272df4e7232664043e612f2
Gzoref <gzoref@gmail.com>
2021-01-07 18:07:19 -0500

    Eslint fix

--- 0x12-Node_JS_basic/2-read_file.js
+++ 0x12-Node_JS_basic/2-read_file.js
@@ -1,14 +1,14 @@
-const fs = require("fs");
+const fs = require('fs');
 const readline = require('readline');
 
 function countStudents(database) {
   try {
     const readLine = fs.readFileSync(database, 'utf8');
     const lines = readLine.split(/\r?\n/);
-    process.stdout.write(`Number of students: ${readLine}`)
+    process.stdout.write(`Number of students: ${readLine}`);
   } catch (error) {
-    throw new Error("Cannot load the database");
+    throw new Error('Cannot load the database');
   }
-};
+}
 
 module.exports = countStudents;
