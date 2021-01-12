32838a57cf405b14db1151643938edc90e7c9242
Gzoref <gzoref@gmail.com>
2021-01-12 15:34:18 -0500

    Only use assert

--- 0x13-unittests_in_js/0-calcul.test.js
+++ 0x13-unittests_in_js/0-calcul.test.js
@@ -2,27 +2,10 @@ const assert = require("assert");
 const calculateNumber = require("./0-calcul");
 
 describe("calculateNumber", function () {
-  describe("Two numbers", function () {
-    it("should return 4", function () {
-      assert.equal(calculateNumber(1, 3), 4);
-    });
-  });
-
-  describe("one rounded number", function () {
-    it("should return 5", function () {
-      assert.equal(calculateNumber(1, 3.7), 5);
-    });
-  });
-
-  describe("two rounded numbers", function () {
-    it("should return 5", function () {
-      assert.equal(calculateNumber(1.2, 3.7), 5);
-    });
-  });
-
-  describe("two rounded numbers", function () {
-    it("should return 6", function () {
-      assert.equal(calculateNumber(1.5, 3.7), 6);
-    });
+  it("should return rounded sum", function () {
+    assert.equal(calculateNumber(1, 3), 4);
+    assert.equal(calculateNumber(1, 3.7), 5);
+    assert.equal(calculateNumber(1.2, 3.7), 5);
+    assert.equal(calculateNumber(1.5, 3.7), 6);
   });
 });
