8cc22b90a07c3397e5c26a27a4d602b1142e9b58
Gzoref <gzoref@gmail.com>
2021-01-12 15:42:03 -0500

    Use strictequal

--- 0x13-unittests_in_js/1-calcul.test.js
+++ 0x13-unittests_in_js/1-calcul.test.js
@@ -2,27 +2,19 @@ const assert = require("assert");
 const calculateNumber = require("./1-calcul");
 
 describe("calculateNumber", function () {
-  describe("Two numbers", function () {
-    it("should return 6", function () {
-      assert.equal(calculateNumber("SUM", 1.4, 4.5), 6);
-    });
+  it("should return 6", function () {
+    assert.equal(calculateNumber("SUM", 1.4, 4.5), 6);
   });
+});
 
-  describe("Two numbers", function () {
-    it("should return -4", function () {
-      assert.equal(calculateNumber("SUBTRACT", 1.4, 4.5), 4);
-    });
-  });
+it("should return -4", function () {
+  assert.strictEqual(calculateNumber("SUBTRACT", 1.4, 4.5), 4);
+});
 
-  describe("Two numbers", function () {
-    it("should return 0.2", function () {
-      assert.equal(calculateNumber("DIVIDE", 1.4, 4.5), 0.2);
-    });
-  });
+it("should return 0.2", function () {
+  assert.strictEqual(calculateNumber("DIVIDE", 1.4, 4.5), 0.2);
+});
 
-  describe("Two numbers", function () {
-    it("should return divide by 0 Error", function () {
-      assert.equal(calculateNumber("DIVIDE", 1.4, 0), "Error");
-    });
-  });
+it("should return divide by 0 Error", function () {
+  assert.strictEqual(calculateNumber("DIVIDE", 1.4, 0), "Error");
 });
