5dd0bcf063910921858f1aebd9899a8bfd9bcb9a
Gzoref <gzoref@gmail.com>
2021-01-11 20:13:41 -0500

    Eslint fix

--- 0x12-Node_JS_basic/4-http.js
+++ 0x12-Node_JS_basic/4-http.js
@@ -1,14 +1,14 @@
 const http = require('http');
 
-const host = '127.0.0.1';
+const hostname = '127.0.0.1';
 const port = 1245;
 
-const app = http.createServer((request, result) => {
-  result.statusCode = 200;
-  result.setHeader('Content-Type', 'text/plain');
-  result.end('Hello Holberton School!');
+const app = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello Holberton School!');
 });
 
-app.listen(port, host);
+app.listen(port, hostname);
 
 module.exports = app;
