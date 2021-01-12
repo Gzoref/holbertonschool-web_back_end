44e89eaec912e5b76998f7c9ef253b4fc6d7015c
Gzoref <gzoref@gmail.com>
2021-01-11 21:12:44 -0500

    Reading a file synchronously with Node JS

--- 0x12-Node_JS_basic/2-read_file.js
+++ 0x12-Node_JS_basic/2-read_file.js
@@ -1,13 +1,43 @@
 const fs = require('fs');
-const readline = require('readline');
 
 function countStudents(database) {
+  let students = [];
+  const StudentGroup = {};
+  const studentObj = [];
+
   try {
-    const readLine = fs.readFileSync(database, 'utf8');
-    const lines = readLine.split(/\r?\n/);
-    process.stdout.write(`Number of students: ${readLine}`);
+    students = fs.readFileSync(database, 'utf8');
   } catch (error) {
     throw new Error('Cannot load the database');
+  }
+
+  students = students.split('\n');
+  const header = students.shift().split(',');
+
+  students.forEach((element) => {
+    if (element) {
+      const studentInfo = element.split(',');
+
+      header.forEach((header, index) => {
+        studentObj[header] = studentInfo[index];
+        if (header === 'field') {
+          if (StudentGroup[studentInfo[index]]) {
+            StudentGroup[studentInfo[index]].push(studentObj.firstname);
+          } else {
+            StudentGroup[studentInfo[index]] = [studentObj.firstname];
+          }
+        }
+      });
+      studentObj.push(studentObj);
+    }
+  });
+  console.log(`Number of students: ${studentObj.length}`);
+
+  for (const info in StudentGroup) {
+    if (StudentGroup[info]) {
+      const listStudents = StudentGroup[info];
+      console.log(`Number of students in ${info}: ${listStudents.length}. List: ${listStudents.join(', ')}`);
+    }
   }
 }
 
