817e2e8d0a32144fa9f21d145736e4146abba59f
Gzoref <gzoref@gmail.com>
2021-01-12 15:46:59 -0500

    Simplicy

--- 0x13-unittests_in_js/2-calcul_chai.test.js
+++ 0x13-unittests_in_js/2-calcul_chai.test.js
@@ -3,27 +3,19 @@ const chai = require("chai");
 const expect = chai.expect;
 
 describe("calculateNumber", function () {
-  describe("Two numbers", function () {
-    it("should return 6", function () {
-      chai.expect(calculateNumber("SUM", 1.4, 4.5)).to.equal(6);
-    });
+  it("should return 6", function () {
+    chai.expect(calculateNumber("SUM", 1.4, 4.5)).to.equal(6);
   });
 
-  describe("Two numbers", function () {
-    it("should return -4", function () {
-      chai.expect(calculateNumber("SUBTRACT", 1.4, 4.5)).to.equal(4);
-    });
+  it("should return -4", function () {
+    chai.expect(calculateNumber("SUBTRACT", 1.4, 4.5)).to.equal(4);
   });
 
-  describe("Two numbers", function () {
-    it("should return 0.2", function () {
-      chai.expect(calculateNumber("DIVIDE", 1.4, 4.5)).to.equal(0.2);
-    });
+  it("should return 0.2", function () {
+    chai.expect(calculateNumber("DIVIDE", 1.4, 4.5)).to.equal(0.2);
   });
 
-  describe("Two numbers", function () {
-    it("should return divide by 0 Error", function () {
-      chai.expect(calculateNumber("DIVIDE", 1.4, 0)).to.equal("Error");
-    });
+  it("should return divide by 0 Error", function () {
+    chai.expect(calculateNumber("DIVIDE", 1.4, 0)).to.equal("Error");
   });
 });
