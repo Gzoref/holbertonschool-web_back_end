91ccb9db83056fb0e993cf9b3953f354b7e7addb
Gzoref <gzoref@gmail.com>
2021-01-12 15:44:08 -0500

    Move end of dscribe funtion to end

--- 0x13-unittests_in_js/1-calcul.test.js
+++ 0x13-unittests_in_js/1-calcul.test.js
@@ -5,16 +5,16 @@ describe("calculateNumber", function () {
   it("should return 6", function () {
     assert.equal(calculateNumber("SUM", 1.4, 4.5), 6);
   });
-});
 
-it("should return -4", function () {
-  assert.strictEqual(calculateNumber("SUBTRACT", 1.4, 4.5), 4);
-});
+  it("should return -4", function () {
+    assert.strictEqual(calculateNumber("SUBTRACT", 1.4, 4.5), 4);
+  });
 
-it("should return 0.2", function () {
-  assert.strictEqual(calculateNumber("DIVIDE", 1.4, 4.5), 0.2);
-});
+  it("should return 0.2", function () {
+    assert.strictEqual(calculateNumber("DIVIDE", 1.4, 4.5), 0.2);
+  });
 
-it("should return divide by 0 Error", function () {
-  assert.strictEqual(calculateNumber("DIVIDE", 1.4, 0), "Error");
+  it("should return divide by 0 Error", function () {
+    assert.strictEqual(calculateNumber("DIVIDE", 1.4, 0), "Error");
+  });
 });
